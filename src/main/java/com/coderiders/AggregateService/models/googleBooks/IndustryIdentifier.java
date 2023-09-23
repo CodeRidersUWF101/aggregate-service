785d8f41028c67dba6654cd3652e6d9db7f3ae70
KodyAnderson1 <kody_anderson@yahoo.com>
2023-09-23 18:09:42 -0500

    Added webclients for all BE services and mock data endpoint for BookSearchService

---
+++ src/main/java/com/coderiders/AggregateService/models/googleBooks/IndustryIdentifier.java
@@ -0,0 +1,12 @@
+package com.coderiders.AggregateService.models.googleBooks;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class IndustryIdentifier {
+
+  public String type;
+  public String identifier;
+}
---
+++ src/main/java/com/coderiders/AggregateService/models/googleBooks/SearchInfo.java
@@ -0,0 +1,11 @@
+package com.coderiders.AggregateService.models.googleBooks;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class SearchInfo {
+
+  public String textSnippet;
+}
