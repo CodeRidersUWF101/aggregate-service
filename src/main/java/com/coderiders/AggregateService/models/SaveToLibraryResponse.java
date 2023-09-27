d96abb8a743b394b137e2e16faacda862ba19dcd
KodyAnderson1 <kody_anderson@yahoo.com>
2023-09-27 18:05:13 -0500

    Added flags, tied in user service for add/remove book

--- src/main/java/com/coderiders/AggregateService/models/SaveToLibraryResponse.java
+++ src/main/java/com/coderiders/AggregateService/models/SaveToLibraryResponse.java
@@ -1,8 +1,12 @@
 package com.coderiders.AggregateService.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SaveToLibraryResponse {
     String id;
 }
