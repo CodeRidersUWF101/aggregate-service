141adb7940d25ac6188a93436ec67655bf21bf1a
Dillon Vaughan <djv8@students.uwf.edu>
2023-10-15 17:51:02 -0500

    Working add user challenge endpoint

--- src/main/java/com/coderiders/AggregateService/controllers/AggregateControllerGamification.java
+++ src/main/java/com/coderiders/AggregateService/controllers/AggregateControllerGamification.java
@@ -1,20 +1,33 @@
 package com.coderiders.AggregateService.controllers;
 
 
+import com.coderiders.AggregateService.services.GamificationService;
+import com.coderiders.commonutils.models.User;
+import com.coderiders.commonutils.models.requests.SaveUserChallenges;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/gamification")
 @RequiredArgsConstructor
 public class AggregateControllerGamification {
+
+    private final GamificationService gamificationService;
+
+
+
     @GetMapping("/")
     public String myRoute() {
         log.info("Gamification Base Route Hit");
         return "Successful Gamification AggregateController";
+    }
+
+    @PostMapping("/challenge")
+    public void saveUserChallenge(@RequestBody SaveUserChallenges saveUserChallenges) {
+        log.info("Saving User Challenge with aggregation Service");
+//        log.info("/users/signup POST ENDPOINT HIT: " + user.getClerkId());
+        gamificationService.saveUserChallenge(saveUserChallenges);
     }
 }
---
+++ src/main/java/com/coderiders/AggregateService/services/Impl/GamificationServiceImpl.java
@@ -0,0 +1,55 @@
+package com.coderiders.AggregateService.services.Impl;
+
+import com.coderiders.AggregateService.exceptions.AggregateException;
+import com.coderiders.AggregateService.services.GamificationService;
+import com.coderiders.commonutils.models.UserLibraryWithBookDetails;
+import com.coderiders.commonutils.models.googleBooks.SaveBookRequest;
+import com.coderiders.commonutils.models.requests.SaveUserChallenges;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class GamificationServiceImpl implements GamificationService {
+
+    @Value("${endpoints.gamification.saveChallenge}")
+    private String saveUserChallengesEndpoint;
+
+    private final WebClient webClient;
+
+    public GamificationServiceImpl(@Qualifier("gamificationServiceClient") WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    public void saveUserChallenge(SaveUserChallenges saveUserChallenges) {
+
+
+
+        webClient
+                .post()
+                .uri(saveUserChallengesEndpoint)
+                .bodyValue(saveUserChallenges);
+//                .retrieve()
+//                .onStatus(HttpStatusCode::is4xxClientError, resp -> resp.bodyToMono(String.class)
+//                        .flatMap(errorMessage -> Mono.error(new AggregateException("4xx Response from POST " + saveUserChallengesEndpoint, errorMessage))))
+//                .onStatus(HttpStatusCode::is5xxServerError, resp -> resp.bodyToMono(String.class)
+//                        .flatMap(errorMessage -> Mono.error(new AggregateException("5xx Response from POST " + saveUserChallengesEndpoint, errorMessage))))
+//                .bodyToMono(String.class)
+//                .block();
+
+//        if (res == null) {
+//            throw new AggregateException("Failed to save to Gamification Library");
+//        }
+    }
+
+
+}
