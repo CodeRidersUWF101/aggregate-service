d85563fbd8c9a50f7ffab1c444e8d21d90289bf6
KodyAnderson1 <kody_anderson@yahoo.com>
2023-10-25 15:36:01 -0500

    Updates

--- src/main/java/com/coderiders/AggregateService/utilities/AggregateConstants.java
+++ src/main/java/com/coderiders/AggregateService/utilities/AggregateConstants.java
@@ -5,7 +5,7 @@ public class AggregateConstants {
     public static final String CLERK_ID = "clerk_id";
     public static final String BOOK_ID = "book_id";
     public static final String MAX_RESULTS = "maxResults";
-    public static final String MAX_RESULTS_COUNT = "20";
+    public static final String MAX_RESULTS_COUNT = "40";
     public static final String START_INDEX = "startIndex";
     public static final String QUERY = "q";
     public static final String BASIC_START_INDEX = "0";
